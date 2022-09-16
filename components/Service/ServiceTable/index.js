c3347fb8b8f5be9ec6542ab7f684ba8b83e08516
Tran Long <long.pham.tran2612@gmail.com>
2022-09-16 23:37:49 +0700

    ++

--- components/Service/ServiceTable/index.js
+++ components/Service/ServiceTable/index.js
@@ -3,25 +3,7 @@ import React, { useState, useEffect } from "react";
 import Column from "antd/lib/table/Column";
 import {getServices} from 'api/serviceAPI';
 function ServiceTable(props) {
-	// const {
-	// 	topics,
-	// 	setInitialValue,
-	// 	setIsModalVisible,
-	// 	setIsAddMode,
-	// 	courses,
-	// 	query,
-	// } = props;
 
-	// const handleRedundantProperty = (id) => {
-	// 	const filter = courses.filter((course) => course.id === id);
-	// 	const course = filter ? filter[0] : null;
-	// 	if (course) {
-	// 		delete course.stt;
-	// 		delete course.personNumber;
-	// 		delete course.wordNumber;
-	// 	}
-	// 	return course;
-	// };
 	const [services, setServices] = useState([]);
 
 	const handleGetServices = async () => {
@@ -53,11 +35,11 @@ function ServiceTable(props) {
 				dataIndex="stt"
 				key="stt"
 			/>
-			<Column title="Tên khóa học" dataIndex="name" key="name" />
+			<Column title="MÃ" dataIndex="name" key="name" />
 			<Column
-				title="Chủ đề"
+				title="Mã dịch vụ"
 				dataIndex="topicId"
-				key="topicId"
+				key="id"
 				// render={(_, record) => {
 				// 	const result = topics
 				// 		.filter((topic) => topic.id === record.topicId)
@@ -67,7 +49,7 @@ function ServiceTable(props) {
 				// }}
 			/>
 			<Column
-				title="Số từ"
+				title="Tên dịch vụ"
 				width="80px"
 				dataIndex="wordNumber"
 				key="wordNumber"
@@ -92,21 +74,6 @@ function ServiceTable(props) {
 	);
 }
 
-// CourseTable.propTypes = {
-// 	courses: PropTypes.array,
-// 	topics: PropTypes.array,
-// 	setInitialValue: PropTypes.func,
-// 	setIsModalVisible: PropTypes.func,
-// 	setIsAddMode: PropTypes.func,
-// 	query: PropTypes.object,
-// };
 
-// CourseTable.defaultProps = {
-// 	courses: [],
-// 	topics: [],
-// 	setInitialValue: null,
-// 	setIsModalVisible: null,
-// 	setIsAddMode: null,
-// };
 
 export default ServiceTable;
