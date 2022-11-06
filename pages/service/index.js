d4033e5ad2fccacc7395393e1d214d5c0c09c167
Tran Long <long.pham.tran2612@gmail.com>
2022-11-06 23:39:04 +0700

    ++

--- pages/service/index.js
+++ pages/service/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { Row, Col, Card, Typography } from "antd";
+import { Row, Col, Card, Typography, Space, Button, Input } from "antd";
+import { SearchOutlined, ClearOutlined } from "@ant-design/icons";
 import Image from "next/image";
 import { getServices } from "pages/api/serviceAPI";
 import { useRouter } from "next/router";
@@ -29,33 +30,51 @@ const ServicePage = () => {
   return (
     <>
       <CustomerNavigation />
-      <div
-        style={{ paddingBottom: "20px" }}
-        id="features"
-        className="text-center"
-      >
-        <div className="container">
-          <div style={{ paddingBottom: "10px" }} className="section-title">
-            <h2>Dịch vụ ở VL-CarCare</h2>
-          </div>
-          <Row style={{ overflow: "auto", height: "530px" }} gutter={[16,16]}>
+
+      <div style={{ backgroundColor:'#EDEBEB', minHeight: "80vh"}} id="features" className="text-center">
+        <div style={{padding:'20px'}} className="container">
+          <Row style={{paddingBottom:'20px'}} gutter={16}>
+            <Col span={8}>
+              <Input.Search
+                placeholder="Tìm kiếm"
+                // onChange={(e) => setSearchGlobal(e.target.value)}
+                // onSearch={(value) => setSearchGlobal(value)}
+                // value={searchGlobal}
+              />
+            </Col>
+            <Col span={2}>
+              <Button
+                // onClick={() => setSearchGlobal("")}
+                icon={<ClearOutlined />}
+              >
+                Xóa bộ lọc
+              </Button>
+            </Col>
+          </Row>
+          <Row
+            style={{ overflow: "auto", height: "580px" }}
+            gutter={[16, 16]}
+          >
             {services
               ? services.map((d, i) => (
                   <Col xs={12} sm={12} md={12} lg={6} xl={6}>
                     <Card
                       style={{
-                        borderRadius: "5px",
                         cursor: "pointer",
                         height: "220px",
+                        borderRadius: "5px",
                         boxShadow: "0 0 10px 0 rgba(0,0,0,0.1)",
-                        
                       }}
                       hoverable
-                      cover={<Image  layout='fill' src={d.imageUrl} />}
+                      cover={<Image layout="fill" src={d.imageUrl} />}
                     />
 
                     <Card.Meta
-                      style={{display:"flex",marginTop:"5px",justifyContent:"space-between"}}
+                      style={{
+                        display: "flex",
+                        marginTop: "5px",
+                        justifyContent: "space-between",
+                      }}
                       title={d.name}
                       description={formatMoney(d.servicePrice.price)}
                     />
@@ -65,6 +84,7 @@ const ServicePage = () => {
           </Row>
         </div>
       </div>
+
       <Loading loading={loading} />
     </>
   );
