9c0fa0c793ad3ccbb299009c6502a8394ed514e3
Tran Long <long.pham.tran2612@gmail.com>
2022-12-07 23:17:54 +0700

    ++up

--- pages/api/url.js
+++ pages/api/url.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = 'http://45.119.212.77:5005';
 // export const API_URL = 'http://localhost:5000'
 // export const API_URL = 'https://car-care-kl.herokuapp.com';
 export const FACEBOOK_ID = process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID;
